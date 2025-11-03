03ae9ff239979813325b27ab71e8191ed96d04f1
AbcZoe <ab0928422863@gmail.com>
2025-11-03 10:49:11 +0800

    User Account and Profile Picture

--- app/forms.py
+++ app/forms.py
@@ -1,29 +1,50 @@
-from xml.dom import ValidationErr
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from app.models import User
+from flask_login import current_user
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username',
+    username = StringField('使用者名稱',
                            validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField("Email", validators=[DataRequired(), Email()])
-    password = PasswordField("Password", validators=[DataRequired()])
-    confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Sign Up')
+    email = StringField("電子郵件", validators=[DataRequired(), Email()])
+    password = PasswordField("密碼", validators=[DataRequired()])
+    confirm_password = PasswordField("確認密碼", validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('註冊')
     
     def validate_username(self,username):
         user = User.query.filter_by(username=username.data).first()
         if user:
-            raise ValidationError('That username is taken. Please choose a different one.')
+            raise ValidationError('該使用者名稱已有人使用. 請選擇另一個.')
 
     def validate_email(self,email):
         user = User.query.filter_by(email=email.data).first()
         if user:
-            raise ValidationError('That email is taken. Please choose a different one.')
+            raise ValidationError('該電子郵件已有人使用. 請選擇另一個.')
 
 class LoginForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired(), Email()])
-    password = PasswordField("Password", validators=[DataRequired()])
-    remember = BooleanField("Remember Me")
-    submit = SubmitField('Login')
+    email = StringField("電子郵件", validators=[DataRequired(), Email()])
+    password = PasswordField("密碼", validators=[DataRequired()])
+    remember = BooleanField("記住密碼")
+    submit = SubmitField('登入')
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('使用者名稱',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField("密碼", validators=[DataRequired(), Email()])
+    picture = FileField("更換頭像", validators=[FileAllowed(['jpg','png'])])
+    submit = SubmitField('更新')
+    
+    def validate_username(self,username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('該使用者名稱已有人使用. 請選擇另一個.')
+
+    def validate_email(self,email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('該電子郵件已有人使用. 請選擇另一個.')
+
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
-import re
+import os
+import secrets
+from PIL import Image
 from flask import request, render_template,url_for,flash,redirect
 from app import app, db, bcrypt
-from app.forms import RegistrationForm, LoginForm
+from app.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from app.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -23,7 +25,7 @@ def register():
         user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
-        flash(f'Your account has been created! You are now able to log in', 'success')
+        flash(f'帳號創建成功! 您現在可以登入了', 'success')
         return redirect(url_for('login'))
     return render_template('register.html',title='Register',form=form)
 
@@ -40,7 +42,7 @@ def login():
             next_page = request.args.get('next')
             return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
-            flash('Login Unsuccessful. Please check email and password', 'danger')
+            flash('登入失敗. 請檢查電子郵件或密碼是否輸入錯誤', 'danger')
     return render_template('login.html',title='login',form=form)
 
 @app.route("/logout")
@@ -48,8 +50,35 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-@app.route("/account")
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    f_name, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    
+    output_size = (125,125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    
+    return picture_fn
+
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html',title='account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('帳號內容已更新!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html',title='account', image_file=image_file, form=form)
     
