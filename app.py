bd155c4c4ccd15c5650726dec071192bdca50983
AbcZoe <ab0928422863@gmail.com>
2025-12-15 11:14:25 +0800

    測試SQLAlchemy

--- app.py
+++ app.py
@@ -1,9 +1,35 @@
 from flask import Flask, request,render_template,url_for,flash,redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 from forms import RegistrationForm, LoginForm
 
-app = Flask(__name__)
 
+app = Flask(__name__)
 app.config['SECRET_KEY'] = 'eb5480089fb042e12832c2c09c9b22b9'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    #unique唯一，nullable不得為空
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False) 
+    image_file = db.Column(db.String(20), nullable=False, default='userImage.png')
+    password = db.Column(db.String(60), nullable=False)
+    posta = db.relationship('Post', backref='author', lazy=True) 
+    def __repr__(self):
+        return f"User('{self.username}','{self.email}','{self.image_file}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(20), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    def __repr__(self):
+        return f"Post('{self.title}','{self.date_posted}')"
+
 
 @app.route("/")
 def home():
@@ -24,6 +50,7 @@ def register():
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    print("form.errors:", form.errors)
     if form.validate_on_submit():
         #用來測試已經有帳號的情況(因為我們還沒有要用資料庫)
         if form.email.data == 'admin@blog.com' and form.password.data == 'password':
