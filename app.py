0925b62ccc1008774a9c1ac1d9541cb5d427ff4a
AbcZoe <ab0928422863@gmail.com>
2025-12-15 11:13:36 +0800

    改成多個景點介紹的網頁

--- app.py
+++ app.py
@@ -1,21 +1,37 @@
-from flask import Flask,render_template
+from flask import Flask, request,render_template,url_for,flash,redirect
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'eb5480089fb042e12832c2c09c9b22b9'
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+@app.route("/nightMarket")
+def nightMarket():
+    return render_template("nightMarket.html")
 
-@app.route("/location")
-def location():
-    return render_template("location.html")
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html',title='Register',form=form)
 
-@app.route("/news")
-def news():
-    return render_template("news.html")
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        #用來測試已經有帳號的情況(因為我們還沒有要用資料庫)
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html',title='login',form=form)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='127.0.0.1', port=5000)
